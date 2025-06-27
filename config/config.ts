77f8fc2ae56588ed85bed2d5b4e655a03b481029
gzx_gds_dell <www.wanan499gzx@163.com>
2025-06-27 15:46:21 +0800

    ci: 工作流

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "umi";
 import { routes } from "./routes";
 
 export default defineConfig({
+  publicPath: '/umi-three-demo/', // 仓库名称
+  base: '/umi-three-demo/',       // 路由前缀
   routes: routes,
   plugins: ["@umijs/plugins/dist/react-query"],
   reactQuery: { devtool: false },
